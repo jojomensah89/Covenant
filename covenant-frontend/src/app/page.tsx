5f891cda2cb8d4b7ad32eb727b064d7f886d5754
ebenezermensah <ebenezer.mensah@amalitech.org>
2024-02-14 17:07:33 +0000

    setup the homepage

--- covenant-frontend/src/app/page.tsx
+++ covenant-frontend/src/app/page.tsx
@@ -1,48 +1,58 @@
-'use client'
-
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { LampContainer } from "@/components/ui/lamp";
+import { Button } from "@/components/ui/moving-border";
 
 function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
-
   return (
-    <>
-      <div>
-        <h2>Account</h2>
+    <div className="relative w-full h-full min-h-screen overflow-hidden bg-gray-900 ">
+      <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="py-4 mt-8 text-4xl font-medium tracking-tight text-center text-transparent bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text md:text-7xl"
+        >
+          Covenant
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-4 text-lg font-medium tracking-tight text-center text-transparent bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text md:text-lg"
+        >
+          Make Agreements The Right Way
+        </motion.p>
 
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
+        <motion.div
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-4 text-lg font-medium tracking-tight text-center text-transparent bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text md:text-lg"
+        >
+          <Button
+            borderRadius="1.75rem"
+            className="text-black bg-white dark:bg-slate-900 dark:text-white border-neutral-200 dark:border-slate-800"
           >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
-  )
+            Enter App{" "}
+          </Button>{" "}
+        </motion.div>
+      </LampContainer>{" "}
+    </div>
+  );
 }
 
-export default App
+export default App;
